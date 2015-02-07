6df09c8ddcae8ffa6baefb09d85df188102b74f3
Miłosz Wieczorek <manfred666@gmail.com>
2015-02-07 19:23:51 +0100

    Updated script files

---
+++ MyPortfolio/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace MyPortfolio.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/Scripts/layout")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/knockout-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/Scripts/application")
+                .Include("~/Scripts/App/App.js"));
+        }
+    }
+}
